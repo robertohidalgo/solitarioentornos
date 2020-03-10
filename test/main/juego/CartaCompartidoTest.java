05467b224bc7fc2c6f484c23ff9eaa5c37a80589
Jarjar Binks <jarjarbinks@iespacomolla.es>
2020-03-10 11:14:55 +0100

    Ejemplo de compartición de objetos para todos los test (desaconsejable)

---
+++ test/main/juego/CartaCompartidoTest.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main.juego;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import org.junit.BeforeClass;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+@FixMethodOrder(MethodSorters.JVM)
+
+/**
+ * Totalmente desaconsejable
+ * @author Rober
+ */
+public class CartaCompartidoTest {
+    
+    private static Carta cartaRoja;
+    private static Carta cartaNegra;
+    
+    @BeforeClass
+    public static void beforeClass(){
+        cartaRoja = new Carta(PaloCarta.CORAZONES, 4);
+        cartaNegra = new Carta(PaloCarta.PICAS, 5);
+    }
+    
+    @Test
+    public void testDistintoColor() {
+            assertTrue(cartaRoja.distintoColor(cartaNegra));
+    }
+    
+    @Test
+    public void testEsUnNumeroMenor() {
+            assertTrue(cartaRoja.esUnNumeroMenor(cartaNegra));
+            assertFalse(cartaNegra.esUnNumeroMenor(cartaRoja));
+    }
+    
+    @Test
+    public void testBocaArriba() {
+            assertFalse(cartaRoja.bocaArriba());
+            assertFalse(cartaNegra.bocaArriba());
+    }
+    
+    @Test
+    public void testVoltea() {
+            cartaRoja.voltea();
+            assertTrue(cartaRoja.bocaArriba());
+            cartaNegra.voltea();
+            assertTrue(cartaRoja.bocaArriba());
+            cartaRoja.voltea();
+            assertFalse(cartaRoja.bocaArriba());
+    }   
+    
+    
+    
+}
